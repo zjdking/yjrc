78cc77364ff42658425177a4d670ad0341723578
wangdakai <wangdakai@capinfo.com.cn>
2020-07-24 17:45:25 +0800

    社保信息

--- src/main/java/com/cn/test/yjrc/controller/HandleController.java
+++ src/main/java/com/cn/test/yjrc/controller/HandleController.java
@@ -1,9 +1,14 @@
 package com.cn.test.yjrc.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.cn.test.yjrc.utils.FileToJson;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,4 +27,13 @@ public class HandleController {
         map.put("handlerName","东城审批");
         return map;
     }
+
+
+    @PostMapping("/getPersonalSnapshotSocialSecurityInfo")
+    @ApiOperation(value = "社保")
+    public String getPersonalSnapshotSocialSecurityInfo(@RequestParam String status) throws IOException {
+        JSONObject getEditUnitInfo = FileToJson.getDictionary("getPersonalSnapshotSocialSecurityInfo");
+        return getEditUnitInfo.toString();
+    }
+
 }
